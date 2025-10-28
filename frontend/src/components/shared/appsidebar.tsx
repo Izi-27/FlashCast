98e55d3dab6f0f24fd2ffbc31f4f8ecf34b15ea0
U~Iriamuzu <daviswill264@gmail.com>
2025-10-28 23:00:39 +0100

    ran build on localhost

--- frontend/src/components/shared/appsidebar.tsx
+++ frontend/src/components/shared/appsidebar.tsx
@@ -50,7 +50,7 @@ const AppSidebar: React.FC = () => {
       {isMobile && openMobile && (
         <div
           className="fixed inset-0 bg-black/50 z-40 transition-opacity duration-300"
-          onClick={() => setOpenMobile(false)}
+          onClick={() => toggleSidebar()}
         />
       )}
       <aside
